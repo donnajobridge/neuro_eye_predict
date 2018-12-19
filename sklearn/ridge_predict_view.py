5c236d0acdd963b244b048a0c7530a853905e10e
donnajobridge <donnajb@gmail.com>
2018-12-18 19:15:43 -0600

    ridge regression ready to go

--- sklearn/ridge_predict_view.py
+++ sklearn/ridge_predict_view.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import os.path
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import KFold
 from sklearn.linear_model import RidgeCV
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
@@ -29,19 +29,19 @@ class ridge(object):
                  cond=None,
                  data_path=None,
                  long_csv=None,
-                 label_key=None,
+                 predictor=None,
                  freq_range=None,
                  time_range=None,
-                 dsrate=None,
+                 dsrate=1,
                  freqs = pd.DataFrame(np.logspace(np.log10(3),np.log10(100),num=50), index=np.arange(1,51), columns=['freqs']),
-                 times = pd.DataFrame(np.arange(-750,751,step=2),index=np.arange(1,752), columns=['time']),
                  keep_pow=True,
                  keep_phase=True):
         self.sub = sub
         self.cond=cond
-        self.label_key = label_key
+        self.predictor = predictor
+        self.label_key = 'class'
         self.data_path=data_path
-        self.data_file = [f'{self.data_path}{self.cond}_behav_{sub}.csv']
+        self.data_file = f'{self.data_path}{self.cond}_behav_{sub}.csv'
 
         # specify features to use
         self.keep_pow=keep_pow
@@ -54,47 +54,57 @@ class ridge(object):
         elif not self.keep_pow and self.keep_phase:
             self.features= 'phase'
 
-        self.predcv = self.data_path+self.sub+'_'+self.cond+'_'+self.label_key+'_'+self.features'ridge_predictions.csv'
+        self.predcv = self.data_path+self.sub+'_'+self.cond+'_'+self.predictor+'_'+self.features+'_ridge_predictions.csv'
 
-        self.scores=self.data_path+self.sub+'_'+self.cond+'_'+self.label_key+'_'+ self.features+'ridge_scores.csv'
+        self.scores=self.data_path+self.sub+'_'+self.cond+'_'+self.predictor+'_'+ self.features+'_ridge_scores.csv'
         # parameters for cross-validation
 
         self.nperms = 10
-
+        self.nfold = 5
         # parameters for sklearn inputs
         self.freqs=freqs
         self.freq_inds = [freqs[(freqs['freqs'] >= freq_range[count][0]) & (freqs['freqs'] <= freq_range[count][1])].index.values for count in range(len(freq_range))]
-        self.times=times
-        self.time_inds = [times[(times['time'] >= time_range[count][0]) & (times['time'] <= time_range[count][1])].index.values for count in range(len(time_range))]
 
+        self.times=pd.DataFrame(np.arange(-750,751,step=dsrate*2),
+        index=np.arange(1,752, step=dsrate), columns=['time'])
+
+        if time_range:
+            self.time_inds = [self.times[(self.times['time'] >= time_range[count][0]) & (self.times['time'] <= time_range[count][1])].index.values for count in range(len(time_range))]
+        else:
+            self.time_inds = [[x] for x in self.times.index.values]
 
     def perform_ridgecv(self, permute_flag=True):
         '''perform nested cross validation using LogisticRegressionCV
         loops through each frequency & time range for each model_selection
         outer loop performs kfold split; inner loop searches for best C parameter (using kfold splits) and fits it to withheld test data from the outer loop kfold
         '''
+        kf = KFold(n_splits=self.nfold, shuffle=True)
+
         df = self.make_df()
         df_norm = normalize_data(df, self.keep_pow, self.keep_phase)
         df_ds = downsample_timeseries(df_norm, self.dsrate)
 
         for f in self.freq_inds:
             for t in self.time_inds:
-                print('beginning time:',t,'beginning freq:',f)
+                # print('beginning time:',t,'beginning freq:',f)
 
                 scorelist = []
                 cond_df = pd.DataFrame()
 
-                df = gen_wide_df(df, f, t)
+                df = gen_wide_df(df_ds, f, t)
+                print(df)
+                for train_ind, test_ind in kf.split(df):
 
-                # get data & labels
-                X,y = get_data_and_labels(df, self.label_key, df.index.get_values())
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+                    # data for this fold
+                    X_train,y_train = get_data_and_labels(df, self.label_key, train_ind)
+                    X_test,y_test = get_data_and_labels(df, self.label_key, test_ind)
 
-                ridgecv = initialize_ridgecv(X_train, y_train)
-                # dictionary of values with probabilities from ridgecv
-                probsdict = {'preds':ridgecv.predict(X_test), 'labels':y_test.reset_index(drop=True), 'orig_ind':test_ind, 'start_time':self.times.loc[t[0], 'time'], 'end_time':self.times.loc[t[-1], 'time'], 'start_freq':self.freqs.loc[f[0], 'freqs'], 'end_freq':self.freqs.loc[f[-1],'freqs'], 'features':self.features, 'dsrate':self.dsrate}
+                    ridgecv = initialize_ridgecv(X_train, y_train, self.nfold)
+                    # dictionary of values with probabilities from ridgecv
+                    probsdict = {'preds':ridgecv.predict(X_test), 'labels':y_test.reset_index(drop=True), 'orig_ind':test_ind, 'start_time':self.times.loc[t[0], 'time'], 'end_time':self.times.loc[t[-1], 'time'], 'start_freq':self.freqs.loc[f[0], 'freqs'], 'end_freq':self.freqs.loc[f[-1],'freqs'], 'features':self.features, 'dsrate':self.dsrate}
 
-                cond_df = pd.DataFrame(probsdict)
+                    fold_df = pd.DataFrame(probsdict)
+                    cond_df = pd.concat([cond_df, fold_df])
 
                 if os.path.isfile(self.predcv):
                     cond_df.to_csv(self.predcv, mode='a', header=False)
@@ -105,7 +115,7 @@ class ridge(object):
 
                 # permute class labels and run model to find a null distribution of auc values
                 if permute_flag:
-                    mse_z, fake_mses = run_perm_test(df, real_mse, self.nperms, self.label_key)
+                    mse_z, fake_mses = run_perm_test(df, kf, real_mse, self.nperms, self.label_key, self.nfold)
                     p = 1-((np.sum(real_mse>fake_mses[0])+1)/(fake_mses.shape[0]+1))
                 else:
                     mse_z=None
@@ -129,38 +139,48 @@ class ridge(object):
         """
         # read in file
         df = pd.read_csv(self.data_file)
-        df['class']=1
         cols_to_drop = [f'view{x}' for x in range(1,4)]+['response']
         # subtract loc3 viewing from location of interest
-        df[f'{self.label_key}-view3'] = df[self.label_key] - df['view3']
+        df[self.label_key] = df[self.predictor] - df['view3']
         df.drop(cols_to_drop, axis=1, inplace=True)
         df.reset_index(drop=True, inplace=True)
+
         return df
 
 
-def initialize_ridgecv(X_train, y_train):
+def initialize_ridgecv(X_train, y_train, nfolds):
     ''' sets up model to run ridge regression cross-validation'''
-    alphas = np.array([10,50,100])
-    ridgecv = RidgeCV(alphas=alphas, cv=None, scoring ='neg_mean_squared_error')
+    alphas = np.logspace(1,5, 10)
+    ridgecv = RidgeCV(alphas=alphas, cv=nfolds, scoring ='neg_mean_squared_error')
     ridgecv.fit(X_train, y_train)
     return ridgecv
 
-def run_perm_test(df, real_mse, nperms, label_key):
+def run_perm_test(df, kf, real_mse, nperms, label_key, nfolds):
     ''' creates model using false class labels, finds mse score n times to create null distribution '''
     fake_mses = pd.DataFrame()
     for i in range(0,nperms):
+        all_preds = None
 
-        # get permuted data
-        X,y = get_data_and_labels(df, label_key, df.index.get_values(), permute=True)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        for train_ind, test_ind in kf.split(df):
 
-        ridgecv = initialize_ridgecv(X_train, y_train)
+            # data for this fold
+            X_train,y_train = get_data_and_labels(df, label_key, train_ind, permute=True)
+            X_test,y_test = get_data_and_labels(df, label_key, test_ind, permute=True)
 
-        predict_perm = pd.DataFrame(ridgecv.predict(X_test))
-        predict_perm['labels'] = y_test.reset_index(drop=True)
+            ridgecv = initialize_ridgecv(X_train, y_train, nfolds)
 
-        fake_mses.loc[i,0] = mean_squared_error(predict_perm['labels'],predict_perm[0])
+            predict_fold = pd.DataFrame(ridgecv.predict(X_test))
+            predict_fold['labels'] = y_test.reset_index(drop=True)
+
+            if all_preds is None:
+                all_preds = predict_fold.copy()
+            else:
+                all_preds = pd.concat([all_preds, predict_fold])
+
+            fake_mses.loc[i,0] = mean_squared_error(all_preds['labels'],all_preds[0])
+
     mse_z = ((real_mse - fake_mses.mean())/fake_mses.std()).get_values()
+
     plot_null(fake_mses)
 
     return mse_z, fake_mses
@@ -169,3 +189,4 @@ def plot_null(fake_aucs):
     print(fake_aucs)
     plt.hist(fake_aucs[0])
     plt.show()
+    plt.clf()
--- sklearn/run_ridge_view.py
+++ sklearn/run_ridge_view.py
@@ -7,11 +7,11 @@ for subject in ['ec109']:
     for condition in conds:
         ridge = ridge(sub = subject,
                                 cond = condition,
-                                label_key = 'view1',
+                                predictor = 'view1',
                                 data_path='/Users/drdj/neuro_eye_predict/data/',
-                                freq_range = [(3,8), (8,15), (15,25), (25,60), (60,101)],
-                                time_range = [(-750,-500), (-500,-250), (-250,0), (0,250), (250,500), (500,750)],
-                                dsrate = 15,
+                                freq_range = [(3,8)],# (8,15), (15,25), (25,60), (60,101)],
+                                # time_range = [(-750,-500), (-500,-250), (-250,0), (0,250), (250,500), (500,750)],
+                                dsrate = 10,
                                 keep_pow = False,
                                 keep_phase = True)
-        decoder.perform_cv()
+        ridge.perform_ridgecv()
